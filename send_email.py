5372d3bb4b1061ec96f9539de65468f67d217dd5
gustamarch <gustamarchi@gmail.com>
2024-04-15 10:52:15 -0300

    Add Functions to send email

--- send_email.py
+++ send_email.py
@@ -1,21 +1,18 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "dev.marchioretto@gmail.com"
-password = 'sxcrekjncdnbzuov'
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "dev.marchioretto@gmail.com"
+    password = 'sxcrekjncdnbzuov'
+    receiver = "dev.marchioretto@gmail.com"
+    context = ssl.create_default_context()
 
-receiver = "dev.marchioretto@gmail.com"
-context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-message = """
-Hi!
-How are you?
-Bye!
-"""
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+
